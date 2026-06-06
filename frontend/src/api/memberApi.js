ac928ba1125a60a64cefb54eeddbfaa18fd68b57
macchiato3671 <CaramelMacchiato3671@gmail.com>
2026-06-06 12:46:10 +0900

    feat: login page

--- frontend/src/api/memberApi.js
+++ frontend/src/api/memberApi.js
@@ -3,3 +3,7 @@ import { publicApiClient } from './apiClient'
 export const signup = (requestBody) => {
   return publicApiClient.post('/members', requestBody)
 }
+
+export const signin = (requestBody) => {
+  return publicApiClient.post('/auth/login', requestBody)
+}
