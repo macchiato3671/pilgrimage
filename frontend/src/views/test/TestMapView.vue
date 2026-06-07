3d4f3a0c7ed15d333f5e64ee585eae8a2cbe3b84
Rainbuilt <rbtree0303@gmail.com>
2026-06-07 19:40:06 +0900

    feat: emit map ready event

--- frontend/src/views/test/TestMapView.vue
+++ frontend/src/views/test/TestMapView.vue
@@ -1,12 +1,25 @@
 <template>
   <main>
     <MapComponent 
-      width="50%"
-      height="400px"
+      :width="componentWidth"
+      :height="componentHeight"
+      @ready="handleMapReady"
     />
   </main>
 </template>
 
 <script setup>
 import MapComponent from '@/components/common/MapComponent.vue'
+import { ref } from 'vue'
+
+const componentWidth = ref('50%')
+const componentHeight = ref('400px')
+
+const kakaoSdk = ref(null)
+const kakaoMap = ref(null)
+
+const handleMapReady = ({ kakao, map }) => {
+  kakaoSdk.value = kakao
+  kakaoMap.value = map
+}
 </script>
