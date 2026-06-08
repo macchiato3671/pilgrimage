de81eaba83f8c9f1c2ab94e6adc222d7f3171579
macchiato3671 <CaramelMacchiato3671@gmail.com>
2026-06-08 16:59:40 +0900

    feat: travel plan list

--- frontend/src/api/planApi.js
+++ frontend/src/api/planApi.js
@@ -4,3 +4,11 @@ import { authApiClient } from './apiClient'
 export const makePlan = (requestBody) => {
   return authApiClient.post('/plans', requestBody);
 };
+
+export const getPlans = (requestBody) => {
+  return authApiClient.get('/plans', requestBody);
+};
+
+export const deletePlan = (planId) => {
+  return authApiClient.delete(`/plans/${planId}`);
+};
