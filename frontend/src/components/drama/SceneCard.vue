37f82dfc0ccf30d8c2e29935b317aa4896cfceea
Rainbuilt <rbtree0303@gmail.com>
2026-06-08 13:46:45 +0900

    feat: add drama scene card

---
+++ frontend/src/components/drama/SceneCard.vue
@@ -0,0 +1,38 @@
+<template> 
+  <main class="component">
+    <img class="thumbnail" :src="props.imgUrl" alt="씬 이미지"/>
+    <p>{{ props.name }}</p>
+    <p>{{ props.description }}</p>
+    <p>{{ props.address }}</p>
+    <p>{{ props.latitude }}, {{ props.longitude }}</p>
+  </main>
+</template>
+
+<script setup>
+  // PROPS
+  const props = defineProps({
+    componentW: String,
+    componentH: String,
+
+    name: String,
+    description: String,
+    address: String,
+    latitude: String,
+    longitude: String,
+    imgUrl: String,
+  })
+</script>
+
+<style lang="scss" scoped>
+.component {
+  width:  v-bind('props.componentW');
+  height: v-bind('props.componentH');
+  background-color: lightgray;
+}
+
+.thumbnail {
+  width: 33%;
+  aspect-ratio: 16 / 9;
+  object-fit: cover;
+}
+</style>
---
+++ frontend/src/views/test/TestSceneCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <SceneCard 
+      :component-w = 'componentW'
+      :component-h = 'componentH'
+
+      name = "샌즈 전"
+      description = "와! 언더테일 아시는구나! 샌즈 정.말.쎕.니.다."
+      address = "Last Corridor, Underground, Mt Ebott"
+      latitude="15.578884844"
+      longitude="888.48441557"
+      img-url = "https://i.namu.wiki/i/vi6nCucR_X3W6DSW4loXSpfaxk-bx272VqXH2dh0iaYjVyWRO88VhL-A7dhuycby59svXTy7TA88418oitJWszUkcGyzimBc02bmZ99P9MmE8cbLa8BkwXVwZFWMfPCk-4twrPGl2OYizgG6aPjtwg.png"
+    />
+  </main>
+</template>
+
+<script setup>
+import SceneCard from '@/components/drama/SceneCard.vue'
+import { ref } from 'vue'
+
+const componentW = ref('400px')
+const componentH = ref('200px')
+</script>
