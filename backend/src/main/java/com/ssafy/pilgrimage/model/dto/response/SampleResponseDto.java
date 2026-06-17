8968f080f3dd46e9f4f0a6bcfa996a19489ac12e
rainbuilt <rbtree0303@gmail.com>
2026-06-17 20:53:43 +0900

    chore: add backend actuator dependency

--- backend/src/main/java/com/ssafy/pilgrimage/model/dto/response/SampleResponseDto.java
+++ backend/src/main/java/com/ssafy/pilgrimage/model/dto/response/SampleResponseDto.java
@@ -1,13 +1,11 @@
 package com.ssafy.pilgrimage.model.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-//@AllArgsConstructor
 @Builder
 public class SampleResponseDto {
 
