5d773acee068126696d5ab9043f4355867777694
macchiato3671 <CaramelMacchiato3671@gmail.com>
2026-06-23 12:39:30 +0900

    feat: wishlist delte, drama list, scene search done

--- backend/src/main/java/com/moonback/pilgrimage/controller/WishlistController.java
+++ backend/src/main/java/com/moonback/pilgrimage/controller/WishlistController.java
@@ -10,9 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.moonback.pilgrimage.model.dto.response.WishlistDramaResponseDto;
 import com.moonback.pilgrimage.model.dto.response.WishlistResponseDto;
+import com.moonback.pilgrimage.model.dto.response.WishlistScenePageResponseDto;
+import com.moonback.pilgrimage.model.dto.response.WishlistSceneResponseDto;
 import com.moonback.pilgrimage.model.service.WishlistService;
 
 import lombok.RequiredArgsConstructor;
@@ -42,6 +46,33 @@ public class WishlistController {
 		int memberId = (int)authentication.getPrincipal();
 		
 		WishlistResponseDto response = wishlistService.getWishlist(memberId);
+		
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(response);
+	}
+	
+	@GetMapping("/dramas")
+	public ResponseEntity<WishlistDramaResponseDto> getDrama(){
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		int memberId = (int)authentication.getPrincipal();
+		
+		WishlistDramaResponseDto response = wishlistService.getDrama(memberId);
+		
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(response);
+	}
+	
+	@GetMapping("/dramas/{dramaId}/scenes")
+	public ResponseEntity<WishlistScenePageResponseDto> getScene(
+														@PathVariable int dramaId,
+														@RequestParam int page,
+														@RequestParam int size){
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		int memberId = (int)authentication.getPrincipal();
+		
+		WishlistScenePageResponseDto response = wishlistService.getScene(memberId, dramaId, page, size);
 		
 		return ResponseEntity
 				.status(HttpStatus.OK)
