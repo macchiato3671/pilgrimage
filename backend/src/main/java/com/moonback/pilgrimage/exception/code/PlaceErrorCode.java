64c18f7ba4ca3d3f933af5e53d4fd96a9ffaaa2b
macchiato3671 <CaramelMacchiato3671@gmail.com>
2026-08-12 12:48:14 +0900

    fix: file encoding to utf-8

--- backend/src/main/java/com/moonback/pilgrimage/exception/code/PlaceErrorCode.java
+++ backend/src/main/java/com/moonback/pilgrimage/exception/code/PlaceErrorCode.java
@@ -8,10 +8,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum PlaceErrorCode implements ErrorCode {
-	// 관광�? ?�세 조회
+	// 관광지 상세 조회
 	PLACE_NOT_FOUND(HttpStatus.NOT_FOUND, "Place does not exists."),
 
-	// 관광�? 조건 검??
+	// 관광지 조건 검색
 	REQUIRED_SEARCH_CONDITION(HttpStatus.BAD_REQUEST, "At least one search condition is required."),
 	INVALID_PAGE_REQUEST(HttpStatus.BAD_REQUEST, "Invalid page request."),
 	INVALID_RADIUS_PARAMETER(HttpStatus.BAD_REQUEST, "Invalid radius parameter.");
